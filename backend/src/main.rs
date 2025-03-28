c615d2ed6577fc84f8b19c865cd7d8aa83a6c12b
zanadoman <domanseven@gmail.com>
2025-03-28 16:49:36 +0100

    backend hello world

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("{}", backend::add(1, 2));
+use core::error::Error;
+use std::env;
+
+use axum::{Router, response::Html, routing};
+use tokio::{net::TcpListener, signal};
+use tracing_subscriber::{
+    EnvFilter,
+    fmt::{self, format::FmtSpan},
+    layer::SubscriberExt,
+    util::SubscriberInitExt,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenvy::dotenv()?;
+    tracing_subscriber::registry()
+        .with(EnvFilter::try_from_default_env()?)
+        .with(fmt::layer().with_span_events(FmtSpan::NEW | FmtSpan::CLOSE))
+        .init();
+    let listener = TcpListener::bind(&env::var("APP_ADDRESS")?).await?;
+    tracing::info!("{listener:?}");
+    axum::serve(
+        listener,
+        Router::new().route(
+            "/",
+            routing::get(async || -> Html<&'static str> {
+                Html("<h1>Hello, World!</h1>")
+            }),
+        ),
+    )
+    .with_graceful_shutdown(async { signal::ctrl_c().await.unwrap() })
+    .await?;
+    Ok(())
 }
