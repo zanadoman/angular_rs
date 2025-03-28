b3ff08470abda8f2c888c42e0e361deb7edc4002
zanadoman <domanseven@gmail.com>
2025-03-28 21:33:50 +0100

    added cors layer

--- backend/src/router.rs
+++ backend/src/router.rs
@@ -1,8 +1,11 @@
 use core::error::Error;
 
-use axum::{Router, extract::Request};
+use axum::{Router, extract::Request, http::HeaderValue};
 use sqlx::MySqlPool;
-use tower_http::trace::TraceLayer;
+use tower_http::{
+    cors::{Any, CorsLayer},
+    trace::TraceLayer,
+};
 use tracing::Level;
 
 use crate::services::Authenticator;
@@ -10,6 +13,11 @@ use crate::services::Authenticator;
 #[tracing::instrument(level = "debug", skip(database))]
 pub fn new(database: MySqlPool) -> Result<Router, Box<dyn Error>> {
     Ok(authentication::router()
+        .layer(
+            CorsLayer::new()
+                .allow_origin("http://localhost:4200".parse::<HeaderValue>()?)
+                .allow_headers(Any),
+        )
         .layer(Authenticator::new(database.clone())?)
         .layer(TraceLayer::new_for_http().make_span_with(
             |request: &Request| {
