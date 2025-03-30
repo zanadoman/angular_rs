cae287113ee66355e485c4019192c1d9d1f1de5e
zanadoman <domanseven@gmail.com>
2025-03-30 22:10:36 +0200

    httpclient config

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,12 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch, withNoXsrfProtection } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(withFetch(), withNoXsrfProtection())
   ]
 };
