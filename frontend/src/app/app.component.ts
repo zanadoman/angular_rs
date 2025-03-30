78a4dea1c2f521265d36e8e5d0e4f2f4fa5c9c6f
zanadoman <domanseven@gmail.com>
2025-03-30 15:52:35 +0200

    better logs cleanup

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -18,54 +18,54 @@ export class AppComponent {
   registerForm = this.formBuilder.group({ name: '', password: '' });
 
   register() {
-    console.log('Register request started.')
+    console.log('Register request started.');
     this.httpClient.post(
       `${this.apiUrl}/register`,
       this.registerForm.value
     ).subscribe({
       error: error => {
-        console.log(`${error.error}\nRegister request failed.`)
-        window.alert(`${error.status}: ${error.error}`)
+        console.log(`${error.error}\nRegister request failed.`);
+        window.alert(`${error.status}: ${error.error}`);
       },
       complete: () => {
-        console.log('Register request completed.')
-        window.alert('Register completed.')
+        console.log('Register request completed.');
+        window.alert('Register completed.');
       }
     });
   }
 
   login() {
-    console.log('Login request started.')
+    console.log('Login request started.');
     this.httpClient.post(
       `${this.apiUrl}/login`,
       this.loginForm.value,
       { withCredentials: true }
     ).subscribe({
       error: error => {
-        console.log(`${error.error}\nLogin request failed.`)
-        window.alert(`${error.status}: ${error.error}`)
+        console.log(`${error.error}\nLogin request failed.`);
+        window.alert(`${error.status}: ${error.error}`);
       },
       complete: () => {
-        console.log('Login request completed.')
-        window.alert('Login completed.')
+        console.log('Login request completed.');
+        window.alert('Login completed.');
       }
     });
   }
 
   logout() {
-    console.log('Logout request started.')
+    console.log('Logout request started.');
     this.httpClient.post(
       `${this.apiUrl}/logout`,
       null,
       { withCredentials: true }
     ).subscribe({
       error: error => {
-        console.log(`${error.error}\nLogout request failed.`)
-        window.alert(`${error.status}: ${error.error}`)
+        console.log(`${error.error}\nLogout request failed.`);
+        window.alert(`${error.status}: ${error.error}`);
       },
       complete: () => {
-        console.log('Logout request completed.')
-        window.alert('Logout completed.')
+        console.log('Logout request completed.');
+        window.alert('Logout completed.');
       }
     });
   }
