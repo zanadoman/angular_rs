213400a59cc73e25dad2b518d232ee49b7787609
zanadoman <domanseven@gmail.com>
2025-04-01 11:53:11 +0200

    simplified trace layer

--- backend/src/router.rs
+++ backend/src/router.rs
@@ -2,36 +2,32 @@ use core::error::Error;
 
 use axum::{
     Router,
-    extract::Request,
     http::{HeaderValue, header::CONTENT_TYPE},
 };
 use sqlx::MySqlPool;
-use tower_http::{cors::CorsLayer, trace::TraceLayer};
+use tower_http::{
+    cors::CorsLayer,
+    trace::{DefaultMakeSpan, TraceLayer},
+};
 use tracing::Level;
 
 use crate::Authenticator;
 
-#[tracing::instrument(level = "debug", skip(database))]
-pub fn new(database: MySqlPool) -> Result<Router, Box<dyn Error>> {
+#[tracing::instrument(level = "debug", skip(pool))]
+pub fn new(pool: MySqlPool) -> Result<Router, Box<dyn Error>> {
     Ok(authentication::router()
-        .layer(Authenticator::new(database.clone())?)
+        .layer(Authenticator::new(pool.clone())?)
         .layer(
             CorsLayer::new()
                 .allow_credentials(true)
                 .allow_headers([CONTENT_TYPE])
                 .allow_origin("http://localhost:4200".parse::<HeaderValue>()?),
         )
-        .layer(TraceLayer::new_for_http().make_span_with(
-            |request: &Request| {
-                tracing::span! {
-                    Level::INFO,
-                    "request",
-                    method = %request.method(),
-                    route = %request.uri()
-                }
-            },
-        ))
-        .with_state(database))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().level(Level::INFO)),
+        )
+        .with_state(pool))
 }
 
 mod authentication;
